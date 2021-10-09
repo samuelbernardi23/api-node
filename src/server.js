83792449f7fd248a29a7e7dd7b78157db1033ad8
Samuel Bernardi <47158713+samuelbernardi23@users.noreply.github.com>
2021-10-09 13:39:50 -0300

    add: Cors

--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const routes = require('./routes');
+const cors = require('cors');
+
 require('./database/index');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
