132dae7dc28dd9d52efc3bdf500c47330ee2d034
Samuel Bernardi <47158713+samuelbernardi23@users.noreply.github.com>
2021-10-11 01:37:58 -0300

    add: routes update Pedido

--- src/controllers/PedidoController.js
+++ src/controllers/PedidoController.js
@@ -4,6 +4,14 @@ const Produto = require("../model/Produto");
 const ItemPedido = require("../model/ItemPedido");
 
 module.exports = {
+  async find(req, res) {
+    try {
+      const pedidos = await Pedido.findAll();
+      return res.json(pedidos);
+    } catch {
+      res.json({ message: "Não foi possível encontrar pedidos." });
+    }
+  },
   async store(req, res) {
     const { cliente_id, items } = req.body;
 
@@ -54,16 +62,61 @@ module.exports = {
         .json({ error: `Houve um problema ao cadastrar o pedido` });
     }
   },
-  async find(req, res) {
-    try {
-      const pedidos = await Pedido.findAll();
-      return res.json(pedidos);
-    } catch {
-      res.json({ message: "Não foi possível encontrar pedidos." });
+
+  async update(req, res) {
+    const { pedido_id, items } = req.body;
+
+    const items_pedido = await ItemPedido.findAll({
+      where: { pedido_id: pedido_id },
+    });
+
+    if (items_pedido) {
+      items_pedido.forEach(async (item) => {
+        item.destroy();
+      });
+
+      const valor_total = items
+        .map((i) => i.preco_unitario * i.quantidade)
+        .reduce((i, e) => i + e);
+
+      const pedido = await Pedido.findByPk(pedido_id);
+      pedido.update({valor_total});
+
+      items.map(async (item) => {
+        const { produto_id, quantidade, preco_unitario } = item;
+
+        const produto = await Produto.findByPk(produto_id);
+        if (produto) {
+          const valor_total = quantidade * preco_unitario;
+          const itempedido = await ItemPedido.create({
+            pedido_id,
+            produto_id,
+            quantidade,
+            preco_unitario,
+            valor_total,
+          });
+          const result = await Pedido.findByPk(pedido_id);
+          return res.json(result);
+        } else {
+          res.status(404).json({
+            error: `Produto id (${produto_id}) não foi encontrado.`,
+          });
+        }
+      });
+    } else {
+      res.status(404).json({
+        error: `Items não encontrados para pedido (${pedido_id}) informado.`,
+      });
     }
   },
   async delete(req, res) {
     const { pedido_id } = req.params;
+    const items_pedido = await ItemPedido.findAll({
+      where: { pedido_id: pedido_id },
+    });
+    items_pedido.forEach(async (item) => {
+      item.destroy();
+    });
 
     try {
       const pedido = await Pedido.findByPk(pedido_id);
--- src/routes.js
+++ src/routes.js
@@ -18,6 +18,7 @@ routes.delete("/produtos/:produto_id", ProdutoController.delete);
 
 routes.get("/pedidos", PedidoController.find)
 routes.post("/pedidos", PedidoController.store)
+routes.patch("/pedidos", PedidoController.update)
 routes.delete("/pedidos/:pedido_id", PedidoController.delete);
 
 routes.get("/itempedidos/:pedido_id", ItemPedidoController.find)
