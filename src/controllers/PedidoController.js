9aa2890b21aa9431db429c6925c4bcad05090369
Samuel Bernardi <47158713+samuelbernardi23@users.noreply.github.com>
2021-10-11 11:00:04 -0300

    fix: Insert and update Pedido

--- src/controllers/PedidoController.js
+++ src/controllers/PedidoController.js
@@ -15,12 +15,13 @@ module.exports = {
   async store(req, res) {
     const { cliente_id, items } = req.body;
 
-    const cliente = Cliente.findByPk(parseInt(cliente_id));
+    const cliente = await Cliente.findByPk(cliente_id);
     if (!cliente) {
       return res
         .status(400)
         .json({ error: "Cliente não encontrado na base de dados." });
     }
+
     const valor_total = items
       .map((i) => i.preco_unitario * i.quantidade)
       .reduce((i, e) => i + e);
@@ -28,34 +29,31 @@ module.exports = {
 
     if (pedido) {
       const pedido_id = pedido.dataValues.id;
+      items
+        .map((i) => {
+          Object.assign(i, {
+            valor_total: i.quantidade * i.preco_unitario,
+            pedido_id: pedido_id,
+          });
+          return i.preco_unitario * i.quantidade;
+        })
+        .reduce((i, e) => i + e);
 
-      items.map(async (item) => {
-        const { produto_id, quantidade, preco_unitario } = item;
-
-        const find_pedido = await Pedido.findByPk(pedido_id);
-        if (find_pedido) {
-          const produto = await Produto.findByPk(produto_id);
-          if (produto) {
-            const valor_total = quantidade * preco_unitario;
-            const itempedido = await ItemPedido.create({
-              pedido_id,
-              produto_id,
-              quantidade,
-              preco_unitario,
-              valor_total,
+      try {
+        await ItemPedido.bulkCreate(items)
+          .then((r) => {
+            return res.json({
+              message: "Pedido inserido com sucesso.",
             });
-            return res.json(itempedido);
-          } else {
-            res.status(404).json({
-              error: `Produto id (${produto_id}) não foi encontrado.`,
-            });
-          }
-        } else {
-          res
-            .status(404)
-            .json({ error: `Pedido id (${pedido_id}) não foi encontrado.` });
-        }
-      });
+          })
+          .catch((err) => {
+            res.json(err);
+          });
+      } catch (error) {
+        return res.json({
+          message: "Não foi possivel atualizar os itens do pedido.",
+        });
+      }
     } else {
       res
         .status(404)
@@ -66,64 +64,57 @@ module.exports = {
   async update(req, res) {
     const { pedido_id, items } = req.body;
 
-    const items_pedido = await ItemPedido.findAll({
-      where: { pedido_id: pedido_id },
-    });
+    if (!pedido_id) {
+      return res.json({ message: "Pedido id não informado" });
+    }
 
-    if (items_pedido) {
-      items_pedido.forEach(async (item) => {
-        item.destroy();
+    const valor_total = items
+      .map((i) => {
+        Object.assign(i, {
+          valor_total: i.quantidade * i.preco_unitario,
+          pedido_id: pedido_id,
+        });
+        return i.preco_unitario * i.quantidade;
+      })
+      .reduce((i, e) => i + e);
+
+    try {
+      await ItemPedido.destroy({
+        where: { pedido_id: pedido_id },
       });
-
-      const valor_total = items
-        .map((i) => i.preco_unitario * i.quantidade)
-        .reduce((i, e) => i + e);
-
       const pedido = await Pedido.findByPk(pedido_id);
-      pedido.update({valor_total});
+      await pedido.update({ valor_total: valor_total });
 
-      items.map(async (item) => {
-        const { produto_id, quantidade, preco_unitario } = item;
-
-        const produto = await Produto.findByPk(produto_id);
-        if (produto) {
-          const valor_total = quantidade * preco_unitario;
-          const itempedido = await ItemPedido.create({
-            pedido_id,
-            produto_id,
-            quantidade,
-            preco_unitario,
-            valor_total,
+      try {
+        await ItemPedido.bulkCreate(items)
+          .then((r) => {
+            return res.json({
+              message: "Pedido atualizado com sucesso.",
+            });
+          })
+          .catch((err) => {
+            res.json(err);
           });
-          const result = await Pedido.findByPk(pedido_id);
-          return res.json(result);
-        } else {
-          res.status(404).json({
-            error: `Produto id (${produto_id}) não foi encontrado.`,
-          });
-        }
-      });
-    } else {
-      res.status(404).json({
-        error: `Items não encontrados para pedido (${pedido_id}) informado.`,
-      });
+      } catch (error) {
+        return res.json({
+          message: "Não foi possivel atualizar os itens do pedido.",
+        });
+      }
+    } catch (error) {
+      return res.json({ message: "Não foi possível atualizar o pedido." });
     }
   },
   async delete(req, res) {
     const { pedido_id } = req.params;
-    const items_pedido = await ItemPedido.findAll({
-      where: { pedido_id: pedido_id },
-    });
-    items_pedido.forEach(async (item) => {
-      item.destroy();
-    });
-
     try {
-      const pedido = await Pedido.findByPk(pedido_id);
-      pedido.destroy();
-      return res.json(pedido);
-    } catch {
-      res.json({ message: "Não foi possível deletar o pedido." });
+      await ItemPedido.destroy({ where: { pedido_id } });
+      await Pedido.destroy({ where: { id: pedido_id } });
+
+      return res.json({
+        message: "Pedido excluído com sucesso.",
+      });
+    } catch (error) {
+      return res.json({ message: "Falha ao excluir o item do pedido." });
     }
   },
 };
