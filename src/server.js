29907ec12b33fc79f09290bf13892fb424dfeb17
Samuel Bernardi <47158713+samuelbernardi23@users.noreply.github.com>
2021-11-06 19:07:36 -0300

    fix: Path dabase

--- src/server.js
+++ src/server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const routes = require('./routes');
 const cors = require('cors');
 
-require('./database/index');
+require(__dirname + '/database/index');
+
 const PORT = process.env || 3333;
 const app = express();
 
