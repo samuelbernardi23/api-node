94aacb4684961a46493eb5ee5061fa9bb6371011
Samuel Bernardi <47158713+samuelbernardi23@users.noreply.github.com>
2021-11-06 12:11:53 -0300

    fix: correções Cliente test

--- test/cliente.test.js
+++ test/cliente.test.js
@@ -6,17 +6,15 @@ var expect = chai.expect;
 
 const url = BASE_URL + "/clientes";
 
-describe("Cliente", function () {
+describe("Cliente", function() {
   let idCliente; //Id que será inserido após criar um cliente
   let cliente = {
     nome: "Novo cliente" // Nome que será inserido ao criar
   };
 
-  it('deve criar um ciente corretamente', function async(done) {
+  it('deve criar um cl iente corretamente', function(done) {
 
-    request.post({
-      url, json: true, body: cliente
-    }, (error, response, body) => {
+    request.post({ url, json: true, body: cliente }, (error, response, body) => {
       idCliente = body.id;
       expect(body).to.have.property('id');
       expect(body).to.have.property('nome');
@@ -27,7 +25,7 @@ describe("Cliente", function () {
 
   });
 
-  it("deve retornar uma lista de clientes", function async(done) {
+  it("deve retornar uma lista de clientes", function(done) {
     request.get(url, (error, response, body) => {
       body = JSON.parse(body);
       const cliente = body[0];
@@ -41,7 +39,7 @@ describe("Cliente", function () {
     });
   });
 
-  it("deve alterar um cliente", function async(done) {
+  it("deve alterar um cliente", function(done) {
     request.patch({ url, json: true, body: { id: idCliente, nome: "Novo cliente alterado" } }, (error, response, body) => {
       expect(body.id).to.be.equal(idCliente);
       expect(body.nome).to.be.not.equal(cliente.nome);
@@ -50,7 +48,7 @@ describe("Cliente", function () {
     });
   });
 
-  it("deve exlcuir o cliente inserido", function async(done) {
+  it("deve exlcuir o cliente inserido", function(done) {
     request.delete(`${url}/${idCliente}`, {}, (error, response, body) => {
       body = JSON.parse(body);
 
@@ -60,7 +58,7 @@ describe("Cliente", function () {
     });
   });
 
-  it("deve não encontrar o cliente anteriormente removido", function async(done) {
+  it("deve não encontrar o cliente anteriormente removido", function(done) {
     request.get(`${url}?id=${idCliente}`, {}, (error, response, body) => {
       body = JSON.parse(body);
       
