364af7f1e9ac9bb4f30862a33d1415b458a7588f
Samuel Bernardi <47158713+samuelbernardi23@users.noreply.github.com>
2021-11-06 23:26:43 -0300

    fix: PORT

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 
 require(__dirname + '/database/index');
 
-const PORT = process.env || 3333;
+const PORT = process.env.PORT || 3333;
 const app = express();
 
 app.use(cors());
